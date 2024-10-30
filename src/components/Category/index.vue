76c40b9ca34b823c4b334485be76d704a42c1fd3
Mr-East <113604025+Mr-East@users.noreply.github.com>
2024-10-30 16:18:40 +0800

    属性管理

---
+++ src/components/Category/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-card>
+      <el-form :inline="true">
+        <el-form-item label="一级分类">
+          <el-select>
+            <el-option v-for="(item,index) in c1Arr" :key="item.id" :label="item.name"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="二级分类">
+          <el-select>
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="三级分类">
+          <el-select>
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { reqC1 } from '../../api/product/attr';
+
+const c1Arr = ref<any[]>([])
+const getC1 = async () =>{
+  let res = await reqC1()
+  if(res.code==200){
+   
+    
+    c1Arr.value = res.data
+  }
+}
+onMounted(() => {
+  
+
+  getC1()
+})
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>attr</div>
+  <div>
+    <Category/>
+    <el-card style="margin-top: 10px">
+      <el-button type="primary" size="default" @click="">添加属性</el-button>
+
+      <el-table>
+        <el-table-column label="序号" type="index" align="center" width="80px" />
+        <el-table-column label="属性名称" width="120px" />
+        <el-table-column label="属性值名称" />
+        <el-table-column label="操作" width="120px" />
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref  } from 'vue';
+import Category from '@/components/Category/index.vue'
+
+
+
+</script>
 
 <style scoped lang="scss"></style>
